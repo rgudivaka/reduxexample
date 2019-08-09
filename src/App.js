c7fac5d8463d952aad1d4a0370226cce302d210c
Rishi Gudivaka <rishi.gudivaka@gmail.com>
2019-08-09 00:49:15 -0500

    finished posts app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Provider } from "react-redux";
 import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(data => console.log(data));
+    this.props.fetchPosts();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
+    }
+  }
   render() {
+    const postItems = this.props.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object
+};
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(Posts);
